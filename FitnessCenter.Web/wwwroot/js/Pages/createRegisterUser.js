fc921b6df55d42d96c8b4e1e67dd7c382006e905
Diego Nuñez <nunezbrenesl@gmail.com>
2024-07-12 20:22:06 -0600

    prueba de main

---
+++ FitnessCenter.Web/wwwroot/js/Pages/createRegisterUser.js
@@ -0,0 +1,31 @@
+﻿const createRegisterUser = (e) => {
+    e.preventDefault()
+    const userDetails = {}
+
+    userDetails.Cedula = $("#").val()
+    userDetails.Nombre = $("#").val()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //public string ? Nombre { get; set; }
+   //     public string ? FirstLastName { get; set; }
+   //     public string ? SecondLastName { get; set; }
+   //     public string ? Phone { get; set; }
+   //     public string ? Email { get; set; }
+   //     public string ? RoleName { get; set; }
