3d102f76ccbb1bc08ac1042249288ae3271a3e52
estgarri <65463201+estgarri@users.noreply.github.com>
2024-07-13 10:50:25 -0600

    Reset password

--- FitnessCenter.Data/Crud/UserCrudFactory.cs
+++ FitnessCenter.Data/Crud/UserCrudFactory.cs
@@ -42,5 +42,35 @@ namespace FitnessCenter.Data.Crud
         {
             throw new NotImplementedException();
         }
+
+        public Dictionary<string, string> RetrieveByEmail(string email)
+        {
+            SqlOperation operation = mapper.GetRetrieveByEmailStatement(email);
+            var result = dao.ExecuteStoredProcedureWithResult(operation);
+
+            if (result.Count == 0)
+            {
+                throw new Exception("No response from stored procedure.");
+            }
+
+            var firstRow = result[0];
+            var response = new Dictionary<string, string>();
+
+            if (firstRow.ContainsKey("Message"))
+            {
+                response["Message"] = firstRow["Message"].ToString();
+            }
+            if (firstRow.ContainsKey("OTPCode"))
+            {
+                response["OTPCode"] = firstRow["OTPCode"].ToString();
+            }
+            if (firstRow.ContainsKey("OTPState"))
+            {
+                response["OTPState"] = firstRow["OTPState"].ToString();
+            }
+
+            return response;
+        }
+
     }
 }
