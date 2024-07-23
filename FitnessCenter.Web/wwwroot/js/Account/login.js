fe35b2b70620bd695b23e340a43e63b26c56772e
JhonBsA <brandon.serrano585@gmail.com>
2024-07-22 23:56:17 -0600

    Se agrega el logo, un titulo y tambien alertas basicas para los espacios vacios

---
+++ FitnessCenter.Web/wwwroot/js/Account/login.js
@@ -0,0 +1,22 @@
+﻿import Swal from 'sweetalert2';
+
+const initializeLoginForm = () => {
+    $('#loginForm').on('submit', function (e) {
+        e.preventDefault(); // Previene el envío del formulario
+
+        const email = $('#email').val().trim();
+        const password = $('#password').val().trim();
+
+        if (email === '' || password === '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Todos los campos son obligatorios!',
+            });
+        } else {
+            this.submit(); // Enviar el formulario si todos los campos están llenos
+        }
+    });
+};
+
+$(document).ready(initializeLoginForm);
