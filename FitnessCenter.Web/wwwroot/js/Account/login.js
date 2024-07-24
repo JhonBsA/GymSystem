304ae06a1b6fc1fc230eb68684a26c3f748f54f0
JhonBsA <brandon.serrano585@gmail.com>
2024-07-23 19:59:41 -0600

    Se agrega sweetalert y se prepara para la conexion con sql

--- FitnessCenter.Web/wwwroot/js/Account/login.js
+++ FitnessCenter.Web/wwwroot/js/Account/login.js
@@ -1,11 +1,10 @@
-﻿import Swal from 'sweetalert2';
-
-const initializeLoginForm = () => {
+﻿
+$(document).ready(function () {
     $('#loginForm').on('submit', function (e) {
         e.preventDefault(); // Previene el envío del formulario
 
-        const email = $('#email').val().trim();
-        const password = $('#password').val().trim();
+        var email = $('#email').val().trim();
+        var password = $('#password').val().trim();
 
         if (email === '' || password === '') {
             Swal.fire({
@@ -13,10 +12,39 @@ const initializeLoginForm = () => {
                 title: 'Oops...',
                 text: 'Todos los campos son obligatorios!',
             });
-        } else {
-            this.submit(); // Enviar el formulario si todos los campos están llenos
-        }
-    });
-};
+            return;
 
-$(document).ready(initializeLoginForm);
+        } 
+
+        $.ajax({
+            type: 'POST',
+            url: '/Account/Login',
+            contentType: 'application/json',
+            data: JSON.stringify({ email: email, password: password }),
+            success: function (response) {
+                if (response.success) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Éxito',
+                        text: response.message,
+                    }).then(function () {
+                        window.location.href = '/Home/Index'; // Redirige a la página de inicio o dashboard
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: response.message,
+                    });
+                }
+            },
+            error: function () {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Ocurrió un error al procesar la solicitud',
+                });
+            }
+        });
+    });
+});
