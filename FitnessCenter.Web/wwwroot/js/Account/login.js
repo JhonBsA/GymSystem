18c79a2ae0d27e63e94f8aaabb4326a9bda15c6f
JhonBsA <brandon.serrano585@gmail.com>
2024-07-24 20:49:51 -0600

    Login completamente funcional, se modifica el controller y se comenta el viejo, funciona de igual manera en el swagger

--- FitnessCenter.Web/wwwroot/js/Account/login.js
+++ FitnessCenter.Web/wwwroot/js/Account/login.js
@@ -3,22 +3,22 @@ const loginUser = (e) => {
     e.preventDefault();
 
     //ENVIO POR MEDIO DE OBJETO JSON
-    //const login = {
-    //    email: $("#email").val().trim(),
-    //    password: $("#password").val().trim()
-    //}
+    const login = {
+        email: $("#email").val().trim(),
+        password: $("#password").val().trim()
+    }
 
-    //const apiUrl = API_URL_BASE + "/Account/Login";
+    const apiUrl = API_URL_BASE + "/Account/Login";
 
-    const email = $("#email").val().trim();
-    const password = $("#password").val().trim();
+    //const email = $("#email").val().trim();
+    //const password = $("#password").val().trim();
 
-    const apiUrl = API_URL_BASE + `/Account/Login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+    //const apiUrl = API_URL_BASE + `/Account/Login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
 
     $.ajax({
         url: apiUrl,
         method: "POST",
-        /*data: JSON.stringify(login),*/
+        data: JSON.stringify(login),
         contentType: "application/json;charset=utf-8",
         dataType: "json",
     })
@@ -32,9 +32,15 @@ const loginUser = (e) => {
         })
         .fail((jqXHR, textStatus, errorThrown) => {
             console.error("Error en la solicitud:", textStatus, errorThrown);
+            let errorMessage = "Hubo un problema con el inicio de sesión. Por favor, intente nuevamente.";
+
+            if (jqXHR.responseJSON && jqXHR.responseJSON.Message) {
+                errorMessage = jqXHR.responseJSON.Message;
+            }
+
             Swal.fire({
                 title: "Error",
-                text: "Hubo un problema con el inicio de sesión. Por favor, intente nuevamente.",
+                text: errorMessage,
                 icon: "error",
                 confirmButtonText: "Aceptar",
             });
