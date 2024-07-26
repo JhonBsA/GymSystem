5c121e9bec99257d5641884fe9cd746245845075
JhonBsA <brandon.serrano585@gmail.com>
2024-07-26 14:56:52 -0600

    Se agregan nuevas vistas

---
+++ FitnessCenter.Web/Models/Role/AssignRoleAccessViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCenter.Web.Models.Role
+{
+    public class AssignRoleAccessViewModel
+    {
+        [Display(Name = "IdRol")]
+        public int RoleId { get; set; }
+        [Display(Name = "Nombre del Rol")]
+        public string RoleName { get; set; }
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+        public bool HasAccess { get; set; } // Para determinar si el acceso está asignado
+    }
+}
---
+++ FitnessCenter.Web/Models/Role/RoleAccessViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCenter.Web.Models.Role
+{
+    public class RoleAccessViewModel
+    {
+        public int AccessId { get; set; }
+
+        [Display(Name = "Acceso")]
+        public string AccessName { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+
+        [Display(Name = "Roles con el Acceso")]
+        public List<string> RolesWithAccess { get; set; }
+    }
+}
