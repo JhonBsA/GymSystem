468fda04c289397e911f6a310c30fa777d00ad87
JhonBsA <brandon.serrano585@gmail.com>
2024-08-05 22:01:57 -0600

    Se agregan cambios visuales y de funcionamiento en CreateAppointment

--- FitnessCenter.Web/wwwroot/js/Appointment/CreateAppointment.js
+++ FitnessCenter.Web/wwwroot/js/Appointment/CreateAppointment.js
@@ -1,22 +1,22 @@
-﻿function saveAppointment() {
-    var appointment = {
-        ClienteId: $('#Cliente').val(),
-        FechaHora: $('#FechaHora').val(),
-        EntrenadorId: $('#Entrenador').val(),
-        DurationInMinutes: $('#DurationInMinutes').val()
-    };
+﻿//function saveAppointment() {
+//    var appointment = {
+//        ClienteId: $('#Cliente').val(),
+//        FechaHora: $('#FechaHora').val(),
+//        EntrenadorId: $('#Entrenador').val(),
+//        DurationInMinutes: $('#DurationInMinutes').val()
+//    };
 
-    $.ajax({
-        url: '/Appointment/CreateAppointment',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(appointment),
-        success: function (response) {
-            window.location.href = '/Appointment/IndexAppointments';
-        },
-        error: function (error) {
-            console.log(error);
-            alert('Error al guardar la cita.');
-        }
-    });
-}
+//    $.ajax({
+//        url: '/Appointment/CreateAppointment',
+//        type: 'POST',
+//        contentType: 'application/json',
+//        data: JSON.stringify(appointment),
+//        success: function (response) {
+//            window.location.href = '/Appointment/IndexAppointments';
+//        },
+//        error: function (error) {
+//            console.log(error);
+//            alert('Error al guardar la cita.');
+//        }
+//    });
+//}
