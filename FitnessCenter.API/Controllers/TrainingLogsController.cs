58ae89adfd9954386890e7ab13b69fab575dfbd0
estgarri <65463201+estgarri@users.noreply.github.com>
2024-08-08 21:23:07 -0600

    TrainingLogs entered and retrieved

---
+++ FitnessCenter.API/Controllers/TrainingLogsController.cs
@@ -0,0 +1,43 @@
+﻿using FitnessCenter.Core;
+using FitnessCenter.DTO.TrainingLogsDTO;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FitnessCenter.API.Controllers
+{
+    [EnableCors("NocheCorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainingLogsController : Controller
+    {
+        private readonly TrainingLogsManager _trainingLogsManager;
+        public TrainingLogsController()
+        {
+            _trainingLogsManager = new TrainingLogsManager();
+        }
+
+        [HttpGet("UserTrainingLogs")]
+
+        public IActionResult UserTrainingLogs(int userId)
+        {
+            var result = _trainingLogsManager.GetTrainingLogsByUserId(userId);
+            if (result == null || result.Count == 0)
+            {
+                return NotFound("No training logs found for this user.");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("AddTrainingLog")]
+        public IActionResult AddTrainingLog(TrainingLogs trainingLog)
+        {
+            var result = _trainingLogsManager.CreateTrainingLogs(trainingLog);
+            if (result.ContainsKey("Message"))
+            {
+                return BadRequest(result["Message"]);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ FitnessCenter.Core/TrainingLogsManager.cs
@@ -0,0 +1,26 @@
+﻿using FitnessCenter.DTO.TrainingLogsDTO;
+using FitnessCenter.Data.Crud.TrainingLogsCRUD;
+
+
+namespace FitnessCenter.Core
+{
+    public class TrainingLogsManager
+    {
+        private readonly TrainingLogsCrudFactory _trainingLogsCrudFactory;
+        public TrainingLogsManager()
+        {
+            _trainingLogsCrudFactory = new TrainingLogsCrudFactory();
+        }
+
+        public Dictionary<string, string> CreateTrainingLogs(TrainingLogs trainingLogs)
+        {
+            var result = _trainingLogsCrudFactory.Create(trainingLogs);
+            return result;
+        }
+        public List<TrainingLogs> GetTrainingLogsByUserId(int UserId)
+        {
+            var result = _trainingLogsCrudFactory.RetrieveAll(UserId);
+            return result;
+        }
+    }
+}
